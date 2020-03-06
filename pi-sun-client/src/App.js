50dab16749dd4041e1d42baf70414c550fe760c2
Zulfadhlil Hakim bin Md Iskandar <zul@accordium.com>
2020-03-06 11:18:08 +0800

    remove unused imports

--- pi-sun-client/src/App.js
+++ pi-sun-client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {Row, Col, Layout, Typography, Input, Button} from 'antd';
 import './App.css';
 import CalculatorService from "./services/CalculatorService";
